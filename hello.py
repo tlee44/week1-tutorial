f516cbb2964e961dfa9deae73c19a1c20423ac92
Tyler Lee <tlee44@mail.valenciacollege.edu>
2016-02-19 18:11:08 -0500

    Space line 4
    added end space line 4

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # replace the word 'hector' with your own name
 
 def hello_world():
-	return 'hello world, my name is Tyler Lee'
+	return 'hello world, my name is Tyler Lee '
 
 print( hello_world() + 'Tyler Lee' )
